eca0e2473dc344f3cb3d285e52497b1b18b84c43
technomad21c <brandon.kugsang.jeong@gmail.com>
2019-04-09 07:57:49 -0400

    get data from sensortag

--- gateway-sensortag/sensortag.py
+++ gateway-sensortag/sensortag.py
@@ -32,44 +32,64 @@ class SensorTag(object):
         except Exception:
             print("conection failed!")
         else:
-            print("successfully connected to sensortag...")
+            print("successfully connected")
       
         try:
             self.serviceHumidity  = self.device.getServiceByUUID(self.UUID['humidity']['service'])
-            self.dataHumidity     = self.device.getCharacteristics(self.UUID['humidity']['data'])
-            self.confHumidity     = self.device.getCharacteristics(self.UUID['humidity']['conf'])
-            self.formatHumidity   = self.device.getCharacteristics(self.UUID['humidity']['format'])
+            self.dataHumidity     = self.serviceHumidity.getCharacteristics(self.UUID['humidity']['data'])[0]
+            self.confHumidity     = self.serviceHumidity.getCharacteristics(self.UUID['humidity']['conf'])[0]
+            self.formatHumidity   = self.UUID['humidity']['format']
 
             self.serviceBarometer = self.device.getServiceByUUID(self.UUID['barometer']['service'])
-            self.dataBarometer    = self.device.getCharacteristics(self.UUID['barometer']['data'])
-            self.confBarometer    = self.device.getCharacteristics(self.UUID['barometer']['conf'])
-            self.formatBarometer  = self.device.getCharacteristics(self.UUID['barometer']['format'])
+            self.dataBarometer    = self.serviceBarometer.getCharacteristics(self.UUID['barometer']['data'])[0]
+            self.confBarometer    = self.serviceBarometer.getCharacteristics(self.UUID['barometer']['conf'])[0]
+            self.formatBarometer  = self.UUID['barometer']['format']
 
             self.serviceLight     = self.device.getServiceByUUID(self.UUID['light']['service'])
-            self.dataLight        = self.device.getCharacteristics(self.UUID['light']['data'])
-            self.confLight        = self.device.getCharacteristics(self.UUID['light']['conf'])
-            self.formatLight      = self.device.getCharacteristics(self.UUID['light']['format'])
-
+            self.dataLight        = self.serviceLight.getCharacteristics(self.UUID['light']['data'])[0]
+            self.confLight        = self.serviceLight.getCharacteristics(self.UUID['light']['conf'])[0]
+            self.formatLight      = self.UUID['light']['format']
+ 
+            self.serviceBattery   = self.device.getServiceByUUID(self.UUID['battery']['service'])
+            self.dataBattery      = self.serviceBattery.getCharacteristics(self.UUID['battery']['data'])[0]
+            
         except Exception:
             print("service discovery failed!")
             self.disconnect()
         else:
-            print("successfully connected to " + self.name + "!")
+            print("successfully enabled")
 
     def disconnect(self):
         self.device.disconnect()
 
     def enable(self):
-        self.confHumidity.write(struc.pack("<h", int(self.UUID['humidity']['enable'], 16)))
-        self.confBarometer.write(struc.pack("<h", int(self.UUID['barometer']['enable'], 16)))
-        self.confLight.write(struc.pack("<h", int(self.UUID['light']['enable'], 16)))
+        self.confHumidity.write(struct.pack("B", 0x01))
+        self.confBarometer.write(struct.pack("B", 0x01))
+        self.confLight.write(struct.pack("B", 0x01))
+     
+        '''
+        self.confHumidity.write(struct.pack("<h", int(self.UUID['humidity']['enable'], 16)))
+        self.confBarometer.write(struct.pack("<h", int(self.UUID['barometer']['enable'], 16)))
+        self.confLight.write(struct.pack("<h", int(self.UUID['light']['enable'], 16)))
+        '''
        
     def read(self):
-        humidityValue  = struct.unpack(self.formatHumidity, self.dataHumidity.read()) 
-        barometerValue = struct.unpack(self.formatBarometer,self.dataBaromter.read()) 
-        lightValue     = struct.unpack(self.formatLight, self.dataLight.read())
+        (rawT, rawH)  = struct.unpack(self.formatHumidity, self.dataHumidity.read()) 
+        humiTemp = -46.85 + 175.72 * (rawT / 65536.0)
+        humiRH = -6.0 + 125.0 * ((rawH & 0xFFFC) / 65536.0)
 
-        return (humidityValue, barometerValue, lightValue)
+        (tL, tM, tH, pL, pM, pH) = struct.unpack(self.formatBarometer,self.dataBarometer.read()) 
+        baroTemp = (tH*65536 + tM*256 + tL) / 100.0
+        baroPress = (pH*65536 + pM*256 + pL) / 100.0
+
+        raw = struct.unpack(self.formatLight, self.dataLight.read())[0]
+        m = raw & 0xFFF
+        e = (raw & 0xFF00) >> 12
+        optiValue = 0.01 * (m << 2)
+
+        battery = ord(self.dataBattery.read())
+
+        return (humiTemp,  humiRH, baroTemp, baroPress, optiValue, battery)
 
 if __name__ == "__main__":
     from yamlreader import YamlReader
@@ -83,4 +103,6 @@ if __name__ == "__main__":
     st.setAddress(dict['sensortags'][0]['sensortag']['name'], dict['sensortags'][0]['sensortag']['address']) 
 
     st.connect()
-    #st.enable()
+    st.enable()
+    while True:
+        print("sensor value: ", st.read())
